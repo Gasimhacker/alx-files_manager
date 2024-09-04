dec10c81f68bc6c8f9064195b2c21033ee4e3b6e
Mohamed Ahmed <ma2286572@gmail.com>
2024-09-04 06:10:51 +0000

    Use the correct document name

--- utils/db.js
+++ utils/db.js
@@ -6,7 +6,7 @@ const db = process.env.DB_DATABASE || 'files_manager';
 
 class DBClient {
   constructor() {
-    this.client = new MongoClient(`mongodb://${host}:${port}`, { useUnifiedTopology: true , useNewUrlParser: true});
+    this.client = new MongoClient(`mongodb://${host}:${port}`, { useUnifiedTopology: true });
     this.client.connect().then(() => {
       this.db = this.client.db(db);
     }).catch((err) => {
@@ -25,7 +25,7 @@ class DBClient {
   }
 
   async nbFiles() {
-    const collection = this.db.collection('nbFiles');
+    const collection = this.db.collection('files');
     const count = await collection.countDocuments();
     return count;
   }
